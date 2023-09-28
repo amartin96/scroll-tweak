48a3abb3d91108a6c1565bb51dd26beedde6e9be
Alex Martin <alex.t.martin96@gmail.com>
2023-09-28 00:36:57 -0700

    Stop runloop via selector instead of message port

--- ScrollTweak/main.swift
+++ ScrollTweak/main.swift
@@ -1,14 +1,14 @@
 import AppKit
 
-class EventListener {
+// Must extend NSObject so selectors can be used
+class EventListener: NSObject {
     let thread: Thread
     let eventTap: CFMachPort
     let eventSource: CFRunLoopSource
-    let messagePort: CFMessagePort
-    let messageSource: CFRunLoopSource
     var runloop: CFRunLoop!
 
-    init?() {
+    // Need this dummy parameter because NSObject.init() can't be overridden by init?()
+    init?(_: Void) {
         // Create the scroll wheel event tap
         guard let eventTap = CGEvent.tapCreate(
             tap: .cgSessionEventTap,
@@ -38,80 +38,49 @@ class EventListener {
         }
         self.eventSource = eventSource
 
-        // Create a message port so we can send a stop command to the thread
-        guard let messagePort = CFMessagePortCreateLocal(
-            kCFAllocatorDefault,
-            "" as CFString,
-            { _, _, _, _ in
-                print("Stopping run loop")
-                CFRunLoopStop(CFRunLoopGetCurrent())
-                return nil
-            },
-            nil,
-            nil
-        ) else {
-            print("Failed to create message port")
-            return nil
-        }
-        self.messagePort = messagePort
-
-        guard let messageSource = CFMessagePortCreateRunLoopSource(kCFAllocatorDefault, messagePort, 0)
-        else {
-            print("Failed to create message run loop source")
-            return nil
-        }
-        self.messageSource = messageSource
-
         thread = Thread {
-            print("Run loop thread started: \(Thread.current.name)")
             CFRunLoopAddSource(CFRunLoopGetCurrent(), eventSource, .commonModes)
-            CFRunLoopAddSource(CFRunLoopGetCurrent(), messageSource, .commonModes)
             CFRunLoopRun()
-            print("Run loop stopped, terminating thread")
         }
-        thread.name = UUID().uuidString
         thread.start()
-        print("thread.start() called, isExecuting: \(thread.isExecuting)")
     }
 
     deinit {
-        print("deinit")
-        if CFMessagePortSendRequest(messagePort, 0, nil, 30, 0, nil, nil) != kCFMessagePortSuccess {
-            // TODO failed to send message
-            print("Failed to send message")
-        }
+        self.perform(#selector(stop), on: self.thread, with: nil, waitUntilDone: true)
+    }
+
+    @objc private func stop() {
+        CFRunLoopStop(CFRunLoopGetCurrent())
     }
 }
 
 class App: NSObject, NSApplicationDelegate {
     var eventListener: EventListener?
+
+    // NSStatusBar.system doesn't take ownership of its items, so we must retain ours
     var statusItem: NSStatusItem!
 
-    override init() {
-        print("init")
-        super.init()
-    }
-
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // NSApp.setActivationPolicy(.regular)
-        eventListener = EventListener()
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-        statusItem.button!.title = "+"
-        let menu = NSMenu()
-        menu.addItem(withTitle: "Toggle", action: #selector(toggle), keyEquivalent: "")
-        // menu.addItem(withTitle: "Status", action: #selector(status), keyEquivalent: "")
-        // menu.addItem(withTitle: "Quit", action: #selector(terminate), keyEquivalent: "")
-        statusItem.menu = menu
+        eventListener = EventListener(())
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem.button!.title = "ScrollTweak"
+        statusItem.menu = NSMenu()
+        statusItem.menu!.addItem(withTitle: "Stop", action: #selector(toggle), keyEquivalent: "")
+        statusItem.menu!.addItem(withTitle: "Quit", action: #selector(terminate), keyEquivalent: "")
     }
 
     @objc private func toggle() {
         if eventListener != nil {
-            print("Stopping")
             eventListener = nil
+            statusItem.menu!.item(withTitle: "Stop")?.title = "Start"
         } else {
-            print("Starting")
-            eventListener = EventListener()
+            eventListener = EventListener(())
+            statusItem.menu!.item(withTitle: "Start")?.title = "Stop"
         }
+    }
+
+    @objc private func terminate() {
+        NSApp.terminate(self)
     }
 }
 
